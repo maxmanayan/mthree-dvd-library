3bca3197e09fd563da55dc2c1534ad2c17e29a56
Max Manayan <maximillian521@gmail.com>
2021-09-02 08:57:31 -0600

    react router added and basic styling setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>React DVD Library</h1>
+      <h1 className="App-header">React DVD Library</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
